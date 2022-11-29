a73cc950db9f8ffe4d6aab6d7c6bba4aca873c86
Pukar Sharma <sharmapukar217@gmail.com>
2022-11-29 15:30:18 +0545

    adbc

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+let prisma;
+if(typeof window === "undefined") {
+	prisma = new PrismaClient()
+}
 
 export async function GET() {
-	const users = await prisma.user.findMany()
+	const users = await prisma?.user?.findMany()
 	return new Response(JSON.stringify(users))
 }
