3f3e2e652f939745fcfc86994180add2aa00c256
Pukar Sharma <sharmapukar217@gmail.com>
2022-11-29 15:33:17 +0545

    adbc

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,11 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
+let prisma: PrismaClient | undefined
+
+if (typeof window === "undefined") {
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+  } else {
+    let globalWithPrisma = global as typeof globalThis & {
+      prisma: PrismaClient;
+    };
+    if (!globalWithPrisma.prisma) {
+      globalWithPrisma.prisma = new PrismaClient();
+    }
+    prisma = globalWithPrisma.prisma;
+  }
+}
+
+
 let prisma;
 if(typeof window === "undefined") {
 	prisma = new PrismaClient()
 }
 
 export async function GET() {
-	const users = await prisma?.user?.findMany()
+	let users;
+	if(typeof window === 'undefined') {
+		users = await prisma?.user?.findMany()
+	}
 	return new Response(JSON.stringify(users))
 }
